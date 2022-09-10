8c452c949e8dd9182d1d5c150600df2a45c903e8
Roy Kim <royjayshkim2@gmail.com>
2022-09-10 09:55:22 -0500

    prioritize search scope by inclusion in course id -- course name in descending order

--- src/components/List.js
+++ src/components/List.js
@@ -16,12 +16,13 @@ import "../styles/CourseList.sass";
 
 
 const filterBySubject = (courses, filter_subject) => {
-  return courses.filter(course => course.subject == filter_subject)
+  return courses.filter(course => course.subject.toLowerCase() === filter_subject.toLowerCase())
 }
 const filterBySearch = (courses, filter_search) => {
-  return courses.filter(course => {
-    course.id.includes(filter_search) || course.name.includes(filter_search)
-  })
+  const fs = filter_search.toLowerCase()
+  const in_id = courses.filter(course => course.id.toLowerCase().includes(fs))
+  const in_name = courses.filter(course => course.name.toLowerCase().includes(fs))
+  return in_id.concat(in_name)
 }
 const List = () => {
   const subject_list = require("../data/flat_subject_scrape.json");
@@ -30,12 +31,10 @@ const List = () => {
   const scopeSubject = useSelector(selectScopeSubject);
   const scopeSearch = useSelector(selectScopeSearch);
 
-  // const dispatch = useDispatch();
-
-  
   if (scopeSubject) {
     if (scopeSearch) {
-      
+      return <CourseList 
+      courses={filterBySearch(filterBySubject(course_list, scopeSubject), scopeSearch)}/>
     }
     return <CourseList courses={filterBySubject(course_list, scopeSubject)}/>
   } else if (scopeSearch) {
@@ -49,20 +48,3 @@ const List = () => {
 };
 
 export default List;
-
-/*
-return (
-    <div className="List overflow-auto h-100">
-      {
-        create component called list to add subject_list OR course_list depending on scope state
-        if scopeSubject and scopeCourse are both null -> SubjectList
-        elif scopeCourse -> CourseList
-        
-        if scopeCourse -> CourseList (filtered by scopeCourse onto course id or name)
-        elif scopeSubject -> CourseList (filtered by subject id)
-        else -> SubjectList
-
-      }
-    </div>
-  );
- */
