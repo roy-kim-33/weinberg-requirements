f11dc97fbddd21dc9ab16d28397a439bb4ed29dc
Roy Kim <royjayshkim2@gmail.com>
2022-09-10 09:56:34 -0500

    tying scopeSearch state with value in search bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,32 @@
 import React from 'react'
+import { useSelector, useDispatch } from "react-redux";
+import {
+  emptyScope,
+  setSubject,
+  setSearch,
+  emptySearch,
+  selectScopeSubject,
+  selectScopeSearch,
+} from "../redux/features/scopeSlice";
 
 const NavBar = () => {
+  const dispatch = useDispatch()
+  let handleInputChange = (ev) => {
+    dispatch(setSearch(String(ev.target.value)))
+  }
+  // let handleInputChange = (ev) => console.log(ev.target.value)
+  const scopeSearch = useSelector(selectScopeSearch);
+  const scopeSubject = useSelector(selectScopeSubject);
   return (
     <div className="container-fluid NavBar">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">NU-Req</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon">{/* insert icon here */}</span>
-        </button>
+        <a className="navbar-brand" href="">NU-Req</a>
         <div className="collapse navbar-collapse container-fluid" id="navbarSupportedContent">
           <form className="form-inline my-2 my-lg-0 row">
-            <input className="form-control mr-sm-2 col" type="search" placeholder="Search classes" aria-label="Search" />
+            <input className="form-control mr-sm-2 col" type="search" 
+            placeholder="Search classes" aria-label="Search" 
+            value={scopeSearch?scopeSearch:""}
+            onChange={handleInputChange}/>
           </form>
         </div>
       </nav>
