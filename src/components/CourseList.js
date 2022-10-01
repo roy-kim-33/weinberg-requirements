08583c5213ae6f4d0b9c199765ad1f87a8089076
Roy Kim <royjayshkim2@gmail.com>
2022-09-30 23:20:01 -0500

    added back button to go back to SubjectList from CourseList

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,23 +1,29 @@
 import React from "react";
 import Course from "./Course.js";
+import styles from "../styles/styles.module.sass";
+import { emptyScope } from "../redux/features/scopeSlice.js";
+import { useSelector, useDispatch } from "react-redux";
 
-const CourseList = ({courses}) => {
+const CourseList = ({ courses }) => {
+  const dispatch = useDispatch();
   return (
     <div className="CourseList overflow-auto h-100">
+        <button
+          className={styles.back_button}
+          onClick={() => dispatch(emptyScope())}
+          // onClick={() => console.log("hehe")}
+        >
+          BACK
+        </button>
+
       <h6>COURSES</h6>
-      <hr/>
+      <hr />
       <p>{courses.length} results</p>
-      {
-        courses.map((course, i) => (
-          <Course 
-            key={i} 
-            id={course.id} 
-            name={course.name} 
-          />
-        ))
-      }
+      {courses.map((course, i) => (
+        <Course key={i} id={course.id} name={course.name} />
+      ))}
     </div>
-  )
+  );
 };
 
 export default CourseList;
