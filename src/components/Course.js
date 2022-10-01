9cd008a4b16977b43b3e33943811a9536f56732c
Roy Kim <royjayshkim2@gmail.com>
2022-09-30 23:18:13 -0500

    draggable courses

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,9 +1,38 @@
 import React from "react";
 import styles from "../styles/styles.module.sass";
+import { DragPreviewImage, useDrag } from 'react-dnd'
+import { ItemTypes } from './ItemTypes.js'
 
 const Course = ({ id, name }) => {
+  const [collected, drag, dragPreview] = useDrag(() => ({
+    type: ItemTypes.COURSE,
+    item: { id }
+  }))
   return (
-    <div className="card">
+    collected.isDragging ?
+    <div 
+      className="card"
+      style={{opacity: 0.5}}
+      ref={dragPreview}
+      >
+      <button 
+        className={styles.button}
+        // onClick={() => dispatch(setSubject(Strin(id)))}
+        >
+        <div className="card-body">
+          <h6 className="card-title m-0">
+            {id}
+            <hr/>
+            {name}
+          </h6>
+        </div>
+      </button>
+    </div>
+    :
+    <div 
+      className="card"
+      ref={drag}
+      >
       <button 
         className={styles.button}
         // onClick={() => dispatch(setSubject(Strin(id)))}
